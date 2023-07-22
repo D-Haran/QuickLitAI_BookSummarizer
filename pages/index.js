616b4cf3216f8ccd4635cb6fd540d6ef4557f790
Derrick <derrickr318@yahoo.com>
2023-07-22 14:08:46 -0400

    added error message to api fetch

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,7 @@ export default function Home() {
     const bookTitleAndAuthor = JSON.stringify({
       bookTitleAndAuthor: bookPrompt
     })
+    console(bookTitleAndAuthor)
     try {
       setIsGenerating(true);
     const res = await fetch("/api/createMessage", {
@@ -50,6 +51,7 @@ export default function Home() {
       body: bookTitleAndAuthor,
     });
     setIsGenerating(false);
+    console.log("RES: "+res)
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
